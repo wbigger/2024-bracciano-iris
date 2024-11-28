6209f0719cd37f015c441897d55a252c8d65493d
Claudio <wbigger@gmail.com>
2024-11-28 16:10:44 +0100

    add petal plot

--- plot_iris.py
+++ plot_iris.py
@@ -2,7 +2,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
-
+import pprint
 # Caricamento del dataset
 iris = load_iris()
 X = iris.data
@@ -14,7 +14,10 @@ plt.figure(figsize=(12, 4))
 # Plot per i sepali
 plt.subplot(1, 2, 1)
 for i in range(3):
-    mask = y == i
+    mask = (y == i)
+    print()
+    print("La maschera per il target",i,"è:")
+    print(mask)
     plt.scatter(X[mask, 0], X[mask, 1], label=iris.target_names[i])
 plt.xlabel('Lunghezza sepalo')
 plt.ylabel('Larghezza sepalo')
@@ -22,8 +25,13 @@ plt.title('Distribuzione dei Sepali')
 plt.legend()
 
 # Plot per i petali
-
-# Ora tocca a te! Prova a fare il plot dei petali!
-# Poi fai commit e push
+plt.subplot(1, 2, 2)
+for i in range(3):
+    mask = (y == i)
+    plt.scatter(X[mask, 2], X[mask, 3], label=iris.target_names[i])
+plt.xlabel('Lunghezza petalo')
+plt.ylabel('Larghezza petalo')
+plt.title('Distribuzione dei Petali')
+plt.legend()
 
 plt.show()
