037b7362a9b98d9c3c19e733d6ad553c6607e6a6
Claudio <wbigger@gmail.com>
2024-12-04 16:21:14 +0100

    Add training

--- plot_iris.py
+++ plot_iris.py
@@ -1,5 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -16,8 +18,8 @@ plt.subplot(1, 2, 1)
 for i in range(3):
     mask = (y == i)
     print()
-    print("La maschera per il target",i,"è:")
-    print(mask)
+    # print("La maschera per il target",i,"è:")
+    # print(mask)
     plt.scatter(X[mask, 0], X[mask, 1], label=iris.target_names[i])
 plt.xlabel('Lunghezza sepalo')
 plt.ylabel('Larghezza sepalo')
@@ -34,7 +36,41 @@ plt.ylabel('Larghezza petalo')
 plt.title('Distribuzione dei Petali')
 plt.legend()
 
-plt.show()
+# plt.show()
 
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=26)
 
+# Divido i dati in train e test
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.5,random_state=26)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+# print(X_train_scaled)
+print(f"minimo: {min(X_train_scaled[:,0])}, massimo: {max(X_train_scaled[:,0])}, media: {np.mean(X_train_scaled[:,0])}")
+
+X_test_scaled = scaler.transform(X_test)
+
+mlp = MLPClassifier(
+    hidden_layer_sizes=(4,2),
+    activation="tanh",
+    random_state=99,
+    max_iter=10000
+    )
+
+mlp.fit(X_train_scaled, y_train)
+y_predict = mlp.predict(X_test_scaled)
+
+accuracy = np.mean(y_predict == y_test)
+print(f"Accuratezza: {accuracy:.2f}")
+
+print(f"Test loss: {mlp.loss_}")
+print(f"Numero iterazioni: {mlp.n_iter_}")
+
+# Finalmente, usiamo la nostra rete neurale!!!
+nuovo_iris = [
+    [5.0, 3.5, 1.5, 0.2]
+    ]
+nuovo_iris_scaled = scaler.transform(nuovo_iris)
+previsione_iris = mlp.predict(nuovo_iris_scaled)
+print("Il mio nuovo fiore è.....")
+print("...rullo di tamburi...")
+print(iris.target_names[previsione_iris[0]])
